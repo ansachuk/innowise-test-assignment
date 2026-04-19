8c40065b194a85ad6ceeef6ad925b4341351d060
Andrii Sachuk <ansachuk@icloud.com>
2026-04-19 15:02:30 +0200

    Add search loader, hide theme radios, and update styles

--- src/utils/fetchBooks.js
+++ src/utils/fetchBooks.js
@@ -4,8 +4,16 @@ const headers = new Headers({
 	"User-Agent": "Innowise Book Shelf/1.0 (ansachuk@icloud.com)",
 });
 
+const searchBtnRef = document.querySelector(".search-button");
+const searchLoaderRef = searchBtnRef.firstElementChild;
+const searchIconRef = searchBtnRef.lastElementChild;
+
 const fetchBooks = async q => {
 	const books = [];
+
+	searchBtnRef.setAttribute("disabled", "");
+	searchLoaderRef.classList.remove("hidden");
+	searchIconRef.classList.add("hidden");
 
 	try {
 		const data = await (
@@ -18,6 +26,10 @@ const fetchBooks = async q => {
 	} catch (e) {
 		console.log("e", e);
 	}
+
+	searchBtnRef.removeAttribute("disabled");
+	searchLoaderRef.classList.add("hidden");
+	searchIconRef.classList.remove("hidden");
 
 	return books;
 };
