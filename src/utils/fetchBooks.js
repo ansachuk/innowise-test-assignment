24d06c1174565538fc2ccc32745b5a43584d275c
Andrii Sachuk <ansachuk@icloud.com>
2026-04-20 00:14:48 +0200

    Fixed preflight error on safari

--- src/utils/fetchBooks.js
+++ src/utils/fetchBooks.js
@@ -1,9 +1,5 @@
 export const BASE_URL = "https://openlibrary.org/";
 
-const headers = new Headers({
-	"User-Agent": "Innowise Book Shelf/1.0 (ansachuk@icloud.com)",
-});
-
 const searchBtnRef = document.querySelector(".search-button");
 const searchLoaderRef = searchBtnRef.firstElementChild;
 const searchIconRef = searchBtnRef.lastElementChild;
@@ -16,11 +12,7 @@ const fetchBooks = async q => {
 	searchIconRef.classList.add("hidden");
 
 	try {
-		const data = await (
-			await fetch(BASE_URL + `search.json?fields=title,author_name,first_publish_year,cover_i,author_key&limit=12&q=${q}`, {
-				headers,
-			})
-		).json();
+		const data = await (await fetch(BASE_URL + `search.json?fields=title,author_name,first_publish_year,cover_i,author_key&limit=12&q=${q}`)).json();
 
 		books.push(...data.docs);
 	} catch (e) {
