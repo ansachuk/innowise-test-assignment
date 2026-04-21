53217b848dc8f4dcfbf387a1c48c0c112c631ed6
Andrii Sachuk <ansachuk@icloud.com>
2026-04-21 11:49:42 +0200

    Added simple fav list

--- src/js/handleFavoritesAside.js
+++ src/js/handleFavoritesAside.js
@@ -2,6 +2,31 @@ const refs = {
 	openBtn: document.querySelector(".favorites-open"),
 	backdrop: document.querySelector(".backdrop"),
 	closeBtn: document.querySelector(".favorites-close"),
+	list: document.querySelector(".aside-favorites-list"),
+	deleteBtn: document.querySelector(".aside-delete-favorite-btn"),
+};
+
+const createAsideFavoritesListMarkup = () => {
+	const savedFavorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+	if (savedFavorites.length === 0) {
+		return `<li class="no-favorite">
+						<svg width="80" height="80">
+							<use href="./src/assets/icons.svg#broken-heart"></use>
+						</svg>
+						<p>No favorites...</p>
+					</li>`;
+	}
+
+	return savedFavorites
+		.map(
+			([id, title]) => `<li class="aside-list-item">
+					<img class="aside-cover-image" width="40" height="50" src="https://covers.openlibrary.org/b/olid/${id}-S.jpg" alt="${title} - cover" />
+					<p>${title}</p>
+						<button class="aside-delete-favorite-btn" data-id="${id}" type="button">x</button>
+				</li>`,
+		)
+		.join("");
 };
 
 const handleKeyUp = e => {
@@ -19,6 +44,8 @@ const closeAside = () => {
 };
 
 const openAside = () => {
+	refs.list.innerHTML = createAsideFavoritesListMarkup();
+
 	refs.backdrop.classList.remove("hidden");
 
 	refs.backdrop.addEventListener("click", e => {
